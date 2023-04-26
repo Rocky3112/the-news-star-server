3f2987c2ab1cea86282c9b60815d3a54db856576
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-04-26 23:25:55 +0600

    data loaded from API

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const port = process.env.port || 5000;
-const categories = require('./data/categories.json')
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('The New Dragon is Here!!!')
-})
+app.get("/", (req, res) => {
+  res.send("The New Dragon is Here!!!");
+});
 
-app.get('/categories', (req, res)=>{
-    res.send(categories)
-})
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedNews = news.find((n) => n._id == id);
+  res.send(selectedNews);
+});
+app.get("/categories/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (id === 0) {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
+    res.send(categoryNews);
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
